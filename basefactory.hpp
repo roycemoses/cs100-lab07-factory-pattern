3864b46f9f7efefb5de7d92c9aac2c42b288e514
roycemoses <royce.kmoses@gmail.com>
2020-11-29 20:13:05 -0800

    Declared parse() and updated include paths

--- basefactory.hpp
+++ basefactory.hpp
@@ -1,9 +1,18 @@
 #ifndef __BASE_FACTORY__
 #define __BASE_FACTORY__
 
+#include "add.hpp"
+#include "div.hpp"
+#include "mult.hpp"
+#include "op.hpp"
+#include "pow.hpp"
+#include "rand.hpp"
+#include "sub.hpp"
+
 class BaseFactory
 {
-
+    public:
+        Base* parse(char** input, int length);
 }
 
 #endif // __BASE_FACTORY__
