16e13a5428c0f2ece23d862d422188a7f9d1273e
Danz Moses <danzmoses@gmail.com>
2020-11-30 20:27:52 -0800

    Fixed parse() functionality - Returns nullptr on invalid input - Correctly handles single Op input

--- basefactory.hpp
+++ basefactory.hpp
@@ -16,89 +16,70 @@ class BaseFactory
     public:
         Base* parse(char** input, int length)
         {
-            std::cout << "length: " << length << std::endl;
             Base* result = nullptr;
 
-            // check if the input[1] is a number (cstring of digits)
+            // check if the leftOp is a number (cstring of digits)
             char temp = input[1][0];
             int i = 0;
             do {
                 if (!isdigit(temp))
-                {
-                    std::invalid_argument ia("Invalid argument: Expected a left operand.");
-                    throw ia;
-                }
+                    return nullptr;
                 ++i;
                 temp = input[1][i];
             } while (temp != '\0');
 
-            Base* leftOp = new Op(atoi(input[1]));
-
+            Base* leftOp = new Op(atoi(input[1]));  // set leftOp
             int arrayIndex = 2;
+            if (arrayIndex >= length) // if user only enters a single Op, return leftOp
+                return leftOp;
 
             while (arrayIndex < length)
             {
                 // check that the operator character is 1 character
+                // if not, check that the second character is a '*' for Pow class
+                // otherwise, it is an invalid input
                 if (input[arrayIndex][1] != '\0')
-                {
-                    if (input[arrayIndex][1] != '^')
-                    {
-                        std::invalid_argument ia("Invalid argument. Expected an operator.");
-                        throw ia;
-                    }
-                }
+                    if (input[arrayIndex][1] != '*')
+                        return nullptr;
                 
                 char op = input[arrayIndex][0]; // op is an operator
-
                 ++arrayIndex;
+                if (arrayIndex >= length)       // if there is no right Op, it is invalid input
+                    return nullptr;
 
-                if (arrayIndex >= length)
-                {
-                    std::invalid_argument ia("Invalid argument. Expected a right operand.");
-                    throw ia;
-                }
-
-                // check if the input[arrayIndex] is a number (cstring of digits)
+                // check if the rightOp is a number (cstring of digits)
                 char temp = input[arrayIndex][0];
                 int i = 0;
                 do {
                     if (!isdigit(temp))
-                    {
-                        std::invalid_argument ia("Invalid argument: Expected a left operand.");
-                        throw ia;
-                    }
+                        return nullptr;
                     ++i;
                     temp = input[arrayIndex][i];
                 } while (temp != '\0');
 
-                Base* rightOp = new Op(atoi(input[arrayIndex]));
+                Base* rightOp = new Op(atoi(input[arrayIndex]));    // set rightOp
 
                 if (op == '+')
                     result = new Add(leftOp, rightOp);
                 else if (op == '-')
                     result = new Sub(leftOp, rightOp);
-                else if (op == 'x')
+                else if (op == '*')
                 {
-                    if (input[arrayIndex - 1][1] != '^')
+                    if (input[arrayIndex - 1][1] != '*')
                         result = new Mult(leftOp, rightOp);
                     else
                         result = new Pow(leftOp, rightOp);
                 }
                 else if (op == '/')
                     result = new Div(leftOp, rightOp);
-                else
-                {
-                    std::invalid_argument ia("Invalid argument: Expected an operator.");
-                    throw ia;
-                }
+                else    // if not an operator character, it is invalid input
+                    return nullptr;
 
                 ++arrayIndex;
                 leftOp = result;
             }
-
             return result;
         }
-
 };
 
 #endif // __BASE_FACTORY__
