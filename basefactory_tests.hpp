b232883efe2e22e36ccd1382316d6001c4ae76f2
Danz Moses <danzmoses@gmail.com>
2020-12-02 15:47:57 -0800

    Added MultParseTest for ParseTests

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -21,4 +21,21 @@ TEST(ParseTests, AddParseTest)
     EXPECT_EQ(result->evaluate(), 4);
 }
 
+TEST(ParseTests, MultParseTest)
+{
+    const int LENGTH = 4;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "*",
+        "2"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 * 2.000000");
+    EXPECT_EQ(result->evaluate(), 4);
+}
+
 #endif // BASEFACTORY_TESTS_HPP
