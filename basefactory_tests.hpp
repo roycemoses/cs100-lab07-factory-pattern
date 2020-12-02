edcbf56987e4c50fc1d047ee8d1ce4e49d1aadfb
Danz Moses <danzmoses@gmail.com>
2020-12-02 15:48:38 -0800

    Added PowParseTest for ParseTests

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -38,4 +38,20 @@ TEST(ParseTests, MultParseTest)
     EXPECT_EQ(result->evaluate(), 4);
 }
 
+TEST(ParseTests, PowParseTest)
+{
+    const int LENGTH = 4;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "**",
+        "2"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 ** 2.000000");
+    EXPECT_EQ(result->evaluate(), 4);
+}
 #endif // BASEFACTORY_TESTS_HPP
