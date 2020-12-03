01705c04acc64c6607caa4fc18f15fd9eaa63264
roycemoses <royce.kmoses@gmail.com>
2020-12-02 16:30:41 -0800

    Added PowSubTest

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -212,4 +212,42 @@ TEST(ParseTests, DivPowTest)
     EXPECT_EQ(result->evaluate(), 8000);
 }
 
+TEST(ParseTests, PowAddTest)
+{
+    const int LENGTH = 6;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "**",
+        "4",
+        "+",
+        "3"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 ** 4.000000 + 3.000000");
+    EXPECT_EQ(result->evaluate(), 19);
+}
+
+TEST(ParseTests, PowSubTest)
+{
+    const int LENGTH = 6;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "**",
+        "5",
+        "-",
+        "31"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 ** 5.000000 - 31.000000");
+    EXPECT_EQ(result->evaluate(), 1);
+}
+
 #endif // BASEFACTORY_TESTS_HPP
