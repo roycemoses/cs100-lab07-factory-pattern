34ef831ccc65f22b0211d508503fd9781266b5d5
roycemoses <royce.kmoses@gmail.com>
2020-12-02 16:40:16 -0800

    Added LargeOpTest

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -4,6 +4,21 @@
 #include "gtest/gtest.h"
 #include "basefactory.hpp"
 
+TEST(ParseTests, LargeOpTest)
+{
+    const int LENGTH = 2;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "991827"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "991827.000000");
+    EXPECT_EQ(result->evaluate(), 991827);
+}
+
 TEST(ParseTests, SubTest)
 {
     const int LENGTH = 4;
