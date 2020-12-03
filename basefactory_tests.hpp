775f7c800ef4157a403ff3799ce110e1b65c1916
roycemoses <royce.kmoses@gmail.com>
2020-12-02 16:43:42 -0800

    Added SubTest

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -19,5 +19,21 @@ TEST(ParseTests, LargeOpTest)
     EXPECT_EQ(result->evaluate(), 991827);
 }
 
+TEST(ParseTests, SubTest)
+{
+    const int LENGTH = 4;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "-",
+        "1"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 - 1.000000");
+    EXPECT_EQ(result->evaluate(), 1);
+}
 
 #endif // BASEFACTORY_TESTS_HPP
