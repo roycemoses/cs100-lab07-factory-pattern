630966f6da80a31ed4ef3c59427c1ebdf3fab8e3
roycemoses <royce.kmoses@gmail.com>
2020-12-02 16:46:57 -0800

    Added PowAddTest

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -262,5 +262,24 @@ TEST(ParseTests, PowTest)
     EXPECT_EQ(result->evaluate(), 64);
 }
 
+TEST(ParseTests, PowAddTest)
+{
+    const int LENGTH = 6;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "**",
+        "4",
+        "+",
+        "3"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 ** 4.000000 + 3.000000");
+    EXPECT_EQ(result->evaluate(), 19);
+}
+
 
 #endif // BASEFACTORY_TESTS_HPP
