7a81324af689fa102ccf26e186d7317a4c0192db
roycemoses <royce.kmoses@gmail.com>
2020-12-02 16:13:44 -0800

    Added SubTest

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -4,5 +4,21 @@
 #include "gtest/gtest.h"
 #include "basefactory.hpp"
 
+TEST(ParseTests, SubTest)
+{
+    const int LENGTH = 4;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "-",
+        "1"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 - 1.000000");
+    EXPECT_EQ(result->evaluate(), 1);
+}
 
 #endif // BASEFACTORY_TESTS_HPP
