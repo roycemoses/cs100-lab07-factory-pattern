32e5ca493b2969eb6798307bea4030b5a08ed108
Danz Moses <danzmoses@gmail.com>
2020-12-02 17:28:02 -0800

    Added AddAddParseTest for ParseTests

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -163,4 +163,23 @@ TEST(ParseTests, InvalidInputInvalidRightOpTest)
     EXPECT_TRUE(result == nullptr);
 }
 
+TEST(ParseTests, AddAddParseTest)
+{
+    const int LENGTH = 6;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "+",
+        "2",
+        "+",
+        "4"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 + 2.000000 + 4.000000");
+    EXPECT_EQ(result->evaluate(), 8);
+}
+
 #endif // BASEFACTORY_TESTS_HPP
