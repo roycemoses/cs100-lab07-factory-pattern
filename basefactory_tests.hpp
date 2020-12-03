5929f5b20ad90e9732038ff74796554236da8225
roycemoses <royce.kmoses@gmail.com>
2020-12-02 16:45:17 -0800

    Added SubPowTest

--- basefactory_tests.hpp
+++ basefactory_tests.hpp
@@ -113,5 +113,24 @@ TEST(ParseTests, SubDivTest)
     EXPECT_EQ(result->evaluate(), 1);
 }
 
+TEST(ParseTests, SubPowTest)
+{
+    const int LENGTH = 6;
+    char* input[LENGTH] = {
+        "./calculator.exe",
+        "2",
+        "-",
+        "0",
+        "**",
+        "4"
+    };
+
+    BaseFactory basefactory;
+    Base* result = basefactory.parse(input, LENGTH);
+
+    EXPECT_EQ(result->stringify(), "2.000000 - 0.000000 ** 4.000000");
+    EXPECT_EQ(result->evaluate(), 16);
+}
+
 
 #endif // BASEFACTORY_TESTS_HPP
